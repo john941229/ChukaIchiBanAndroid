ea71f54f563fcf8503c6c1e0ba640bbf97867dcb
程俊翔 <john941229@vip.qq.com>
2016-10-31 10:25:02 +0800

    no message

--- Chuka/app/src/main/java/com/chuka/chuka/MainPage.java
+++ Chuka/app/src/main/java/com/chuka/chuka/MainPage.java
@@ -1,7 +1,13 @@
 package com.chuka.chuka;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainPage extends AppCompatActivity {
 
@@ -9,5 +15,9 @@ public class MainPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+        Toolbar myToolbar = (Toolbar)findViewById(R.id.my_toolbar);
+
+        setSupportActionBar(myToolbar);
+
     }
 }
