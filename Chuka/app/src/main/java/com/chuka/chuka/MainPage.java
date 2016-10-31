adba2ccc0977723b40e5f7f82269dc476eed431f
程俊翔 <john941229@vip.qq.com>
2016-10-31 21:00:42 +0800

    MainPage xml 绘制完了

--- Chuka/app/src/main/java/com/chuka/chuka/MainPage.java
+++ Chuka/app/src/main/java/com/chuka/chuka/MainPage.java
@@ -1,23 +1,35 @@
 package com.chuka.chuka;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainPage extends AppCompatActivity {
 
+    public final static String EXTRA_SEARCH_0="com.chuka.chuka.SEARCH0";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
-        Toolbar myToolbar = (Toolbar)findViewById(R.id.my_toolbar);
 
-        setSupportActionBar(myToolbar);
+        Button typeButton0 = (Button) findViewById(R.id.mainPageTypeButton_0) ;
+
+
+    }
+    public void searchType0(View view){
 
     }
 }
+
