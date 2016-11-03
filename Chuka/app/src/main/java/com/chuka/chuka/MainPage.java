62383ee136da3e0043d5729a5433019cdffb19c7
程俊翔 <john941229@vip.qq.com>
2016-11-03 20:34:53 +0800

    type页布局搞定，按钮内容未做

--- Chuka/app/src/main/java/com/chuka/chuka/MainPage.java
+++ Chuka/app/src/main/java/com/chuka/chuka/MainPage.java
@@ -16,12 +16,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import static android.R.attr.button;
+
 public class MainPage extends AppCompatActivity {
 
     public final static String EXTRA_SEARCH="com.chuka.chuka.SEARCH";
-    public final static String EXTRA_SEARCH_1="com.chuka.chuka.SEARCH1";
-    public final static String EXTRA_SEARCH_2="com.chuka.chuka.SEARCH2";
-    public final static String EXTRA_SEARCH_3="com.chuka.chuka.SEARCH3";
+//    public final static String EXTRA_SEARCH_1="com.chuka.chuka.SEARCH1";
+//    public final static String EXTRA_SEARCH_2="com.chuka.chuka.SEARCH2";
+//    public final static String EXTRA_SEARCH_3="com.chuka.chuka.SEARCH3";
+    int type = 0;
+    public final static String EXTRA_TYPE="com.chuka.chuka.TYPE";
+    String Type = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,29 +37,47 @@ public class MainPage extends AppCompatActivity {
 //        Button typeButton2 = (Button) findViewById(R.id.mainPageTypeButton_2) ;
 //        Button typeButton3 = (Button) findViewById(R.id.mainPageTypeButton_3) ;
 
+
+
     }
     public void searchType0(View view){
         Intent intent = new Intent(this,TypePages.class);
-        int type = 0;
-        intent.putExtra(EXTRA_SEARCH,type);
+        //type = 0;
+
+        Button typeButton0 = (Button) findViewById(R.id.mainPageTypeButton_0) ;
+        Type = typeButton0.getText().toString();
+
+        intent.putExtra(EXTRA_TYPE,Type);
         startActivity(intent);
     }
     public void searchType1(View view){
         Intent intent = new Intent(this,TypePages.class);
-        int type = 1;
-        intent.putExtra(EXTRA_SEARCH,type);
+//        type = 1;
+
+        Button typeButton1 = (Button) findViewById(R.id.mainPageTypeButton_1) ;
+        Type = typeButton1.getText().toString();
+
+        intent.putExtra(EXTRA_TYPE,Type);
+
+//        intent.putExtra(EXTRA_SEARCH,type);
         startActivity(intent);
     }
     public void searchType2(View view){
         Intent intent = new Intent(this,TypePages.class);
-        int type = 2;
-        intent.putExtra(EXTRA_SEARCH,type);
+
+        Button typeButton2 = (Button) findViewById(R.id.mainPageTypeButton_2) ;
+        Type = typeButton2.getText().toString();
+
+        intent.putExtra(EXTRA_TYPE,Type);
         startActivity(intent);
     }
     public void searchType3(View view){
         Intent intent = new Intent(this,TypePages.class);
-        int type = 3;
-        intent.putExtra(EXTRA_SEARCH,type);
+
+        Button typeButton3 = (Button) findViewById(R.id.mainPageTypeButton_3) ;
+        Type = typeButton3.getText().toString();
+
+        intent.putExtra(EXTRA_TYPE,Type);
         startActivity(intent);
     }
     public void callNavigation(View view){
--- Chuka/app/src/main/java/com/chuka/chuka/TypePages.java
+++ Chuka/app/src/main/java/com/chuka/chuka/TypePages.java
@@ -1,12 +1,20 @@
 package com.chuka.chuka;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 /**
@@ -15,33 +23,66 @@ import android.widget.TextView;
 
 public class TypePages extends AppCompatActivity{
 
+    private final Button[] typeButton = new Button[20];
+    private int buttonNum = 0;
+    private int buttonLine = 0;
+    private int buttonExp = 0;
+
     @Override
+
+
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.type_page);
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-
         setSupportActionBar(myToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
         Intent intent = getIntent();
-        String typeName = "";
-        int type = intent.getIntExtra(MainPage.EXTRA_SEARCH,0);
+        String typeName  = intent.getStringExtra(MainPage.EXTRA_TYPE);
 
-        if(type == 0)
-                typeName = "菜肴";
-        if(type == 1)
-                typeName = "主食";
-        if(type == 2)
-                typeName = "甜点";
-        if(type == 3)
-                typeName = "汤类";
-
-        TextView layout = (TextView) findViewById(R.id.display_message);
+        TextView layout = (TextView) findViewById(R.id.title_type_name);
         layout.setText(typeName);
-        layout.setTextSize(40);
+    //  layout.setTextSize(40);
+
+
+        TableLayout layout2 = (TableLayout)findViewById(R.id.types);
+        //layout2.setOrientation(LinearLayout.HORIZONTAL);
+//        setContentView(layout2);
+
+        /////////////////////////
+
+        buttonNum = 5;//获取按钮数
+
+        /////////////////////////
+
+        buttonLine = (buttonNum-1) / 4 +1;
+        buttonExp = buttonNum % 4;
+        int num = buttonNum;
+        int line4 = 4;
+        for(int i=0;i<buttonLine;i++)
+        {
+            if(i == buttonLine -1)
+                line4 = buttonExp;
+
+            TableRow tableRow = new TableRow(this);
+
+            for(int j=0;j<line4 ;j++)
+            {
+                typeButton[num] = new Button(this);
+                typeButton[num].setText("123123");
+                typeButton[num].setBackgroundColor(Color.parseColor("#00000000"));
+                Drawable top = getResources().getDrawable(R.drawable.oval);
+                typeButton[num].setCompoundDrawablesWithIntrinsicBounds(null,top,null,null);
+
+                tableRow.addView(typeButton[num]);
+                num = num+1;
+            }
+            layout2.addView(tableRow);
+        }
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
